02836d2c39cb21888bf595938e116788f04d52de
Lape15 <larape1993@gmail.com>
2020-03-06 12:41:15 +0100

    contact form fixed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,30 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ThemeContext } from "../context/themeContext";
 
 const Contact = () => {
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
   const { isLightTheme, light, dark } = useContext(ThemeContext);
   const theme = isLightTheme ? light : dark;
+
+  const handleChange = e => {
+    setForm({ [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: { "form-name": "contact", ...this.state }
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+  console.log(form);
   return (
     <div id="contact" style={{ background: theme.bg }} className="contact">
-      <form data-netlify="true">
+      <form onSubmit={handleSubmit}>
         <fieldset>
           <legend>Say hi!</legend>
           <div>
@@ -16,6 +34,9 @@ const Contact = () => {
               type="text"
               placeholder="Jane Doe"
               style={{ color: theme.ui }}
+              value={form.name}
+              onChange={handleChange}
+              required
             />
           </div>
           <div>
@@ -24,13 +45,21 @@ const Contact = () => {
               type="email"
               placeholder="email@email.com"
               style={{ color: theme.ui }}
+              value={form.email}
+              onChange={handleChange}
+              required
             />
           </div>
           <div>
             <label style={{ color: theme.ui }} className="label">
               Hit me up
             </label>
-            <textarea placeholder="Drop your messages here"></textarea>
+            <textarea
+              placeholder="Drop your messages here"
+              value={form.message}
+              onChange={handleChange}
+              required
+            ></textarea>
           </div>
 
           <div className="btn">
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "../context/themeContext";
+import landing from "../assets/landing.png";
 
 const Project = () => {
   const { isLightTheme, light, dark } = useContext(ThemeContext);
@@ -8,6 +9,16 @@ const Project = () => {
   return (
     <div style={{ background: theme.bg }} className="projects">
       <h1 style={{ color: theme.ui }}>Holla</h1>
+
+      <div className="project-one">
+        <img src={landing} alt="hi" />
+        <div className="project-one details">
+          <p>Holaaaa</p>
+          <p>Leooooo</p>
+          <p>Looon</p>
+        </div>
+      </div>
+      <div></div>
     </div>
   );
 };
